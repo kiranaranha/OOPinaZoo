5c7c5ab65c9df2254f8695cb6c2b24cada475dbd
kiranaranha <kiranaranha@students.berkeley.net>
2018-10-02 13:22:55 -0700

    h

--- zoo.js
+++ zoo.js
@@ -1,45 +1,10 @@
 function run(){
     let tigger = new Tiger("Tigger");
-    //tigger.eat("meat");
-    //tigger.eat("kibble");
+    tigger.eat("meat");
+
     let pooh = new Bear("Pooh");
-    //pooh.eat("fish");
-    //pooh.eat("meat");
-}
-
-class Tiger {
-    constructor(name) {
-        this.name = name;
-        this.favFood = "meat";
-    }
-
-    sleep() {
-        console.log(this.name + " sleeps for 8 hours");
-
-    }
-
-    eat(food) {
-        console.log(this.name + " eats " + food);
-        food == this.favFood ? console.log("YUM!!! " + this.name + " wants more " + food) : this.sleep();
-
-    }
-
-}
-
-
-class Bear {
-    constructor(name) {
-        this.name = name;
-        this.favFood = "fish";
-    }
-    sleep() {
-        console.log(this.name + " hibernates for 4 months");
-    }
-    eat(food) {
-        console.log(this.name + " eats " + food);
-        food == this.favFood ? console.log("YUM!!! " + this.name + " wants more " + food) : this.sleep();
-
-    }
+    pooh.eat("fish");
+    pooh.eat("meat");
 }
 
 class Animal {
@@ -56,6 +21,39 @@ class Animal {
     eat(food) {
         console.log(this.name + " eats " + food);
         food == this.favFood ? console.log("YUM!!! " + this.name + " wants more " + food) : this.sleep();
+    }
+}
+
+class Tiger extends Animal {
+    constructor(name) {
+        super(name, "meat");
+    }
+}
+
+class Bear extends Animal {
+    constructor(name) {
+        super(name, "fish");
+    }
+    sleep() {
+        console.log(this.name + " hibernates for 4 months");
+    }
+}
+
+class Unicorn extends Animal {
+    constructor(name) {
+        super(name, "marshmallows");
+    }
+    sleep() {
+        console.log(this.name + " sleeps in a cloud");
+    }
+}
+
+class Tiger extends Animal {
+    constructor(name) {
+        super(name, "leaves");
+    }
+    eat(food){
+        food == this.favFood ? console.log("YUM!!! " + this.name + " wants more " + food) : console.log("YUCK!!! " + this.name + " will not eat " + food);
     }
 }
 
