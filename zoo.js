72ba89cf9712e03b389205df55bf58756a4e6c4e
kiranaranha <kiranaranha@students.berkeley.net>
2018-10-03 13:22:58 -0700

    h

--- zoo.js
+++ zoo.js
@@ -5,7 +5,18 @@ function run(){
     let pooh = new Bear("Pooh");
     pooh.eat("fish");
     pooh.eat("meat");
-}
+
+    let rarity = new Unicorn("Rarity");
+    rarity.eat("marshmallows");
+
+    let gemma = new Giraffe("Gemma");
+    gemma.eat("meat");
+    gemma.eat("leaves");
+
+    let stinger = new Bee("Stinger");
+        stinger.eat("ice cream");
+        stinger.eat("pollen");
+    }
 
 class Animal {
     constructor(name, favFood) {
@@ -48,12 +59,36 @@ class Unicorn extends Animal {
     }
 }
 
-class Tiger extends Animal {
+class Giraffe extends Animal {
     constructor(name) {
         super(name, "leaves");
     }
     eat(food){
+        if(food == this.favFood){
+            console.log(this.name + " eats " + food);
+        }
         food == this.favFood ? console.log("YUM!!! " + this.name + " wants more " + food) : console.log("YUCK!!! " + this.name + " will not eat " + food);
+
+    if(food == this.favFood){
+        this.sleep();
+    }
     }
 }
 
+class Bee extends Animal {
+    constructor(name) {
+        super(name, "pollen");
+    }
+
+    eat(food){
+        if(food == this.favFood){
+            console.log(this.name + " eats " + food);
+            console.log("Yum!! "+ this.name + " wants more " + food);
+            let sleep = this.name + " sleeps for 8 hours";
+            console.log(sleep);
+        }else{
+            console.log("Yuck!! "+ this.name + " will not eat " + food);
+            const sleep = this.name + " will never sleep";
+        }
+    }
+}
